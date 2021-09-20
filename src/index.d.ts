dda1e9e441712ea5de655a5757f9565d76254d1c
flazepe <51487578+flazepe@users.noreply.github.com>
2021-09-21 04:56:49 +0800

    fix: typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,8 +2,8 @@ declare module "@flazepe/deezer.js" {
 	export type EntityType = "track" | "album" | "artist" | "playlist";
 
 	export class Deezer {
-		api(method: string, body: Record<string, any>): Promise<Array<Record<string, any>>>;
-		search(query: string, type: EntityType): Promise<Record<string, any>>;
+		api(method: string, body: Record<string, any>): Promise<Record<string, any>>;
+		search(query: string, type: EntityType): Promise<Array<Record<string, any>>>;
 		get(
 			idOrURL: string,
 			type?: EntityType
