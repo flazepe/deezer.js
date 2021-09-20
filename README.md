da597f506b2080855a7cb4b5e8af672d1146d1c7
flazepe <51487578+flazepe@users.noreply.github.com>
2021-09-21 03:27:58 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # deezer.js
+
 A simple package to interact with the Deezer API with track download support.
+
+# Example
+
+```js
+const { Deezer } =  require("deezer.js"),
+	deezer =  new  Deezer();
+
+	(async () => {
+	const track = (await deezer.search("Re Bon Yoyage", "track"))[0];
+
+	// Save track to a file
+	require("fs").writeFileSync(
+		`${track.ART_NAME} - ${track.SNG_TITLE}.mp3`,
+		await deezer.getAndDecryptTrack(track)
+	);
+})();
+```
---
+++ src/index.d.ts
@@ -0,0 +1,15 @@
+import { RequestOptions } from "https";
+
+declare module "deezer.js" {
+	export type EntityType = "track" | "album" | "artist" | "playlist";
+
+	export class Deezer {
+		api(method: string, body: Record<string, any>): Promise<Array<Record<string, any>>>;
+		search(query: string, type: EntityType): Promise<Record<string, any>>;
+		get(
+			idOrURL: string,
+			type?: EntityType
+		): Promise<{ info: Record<string, any>; tracks: Array<Record<string, any>> } | null>;
+		getAndDecryptTrack(track: any): Promise<Buffer>;
+	}
+}
